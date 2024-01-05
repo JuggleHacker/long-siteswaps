6db3e4676c8f59a1e013cf795d31603fcd729e8f
Cameron Ford <cameron.ford@hotmail.co.uk>
2024-01-05 14:29:41 +0000

    Generate patterns made out of 5s, 6s, 7s, 8s and 9s

--- generator.py
+++ generator.py
@@ -2,11 +2,11 @@ from utils import search_multiple_seeds, is_valid
 from approvers import has_pass, no_dragon, no_repeated_pairs
 
 if __name__ == "__main__":
-    throws = [2, 4, 6, 7, 8, 9]
+    throws = [5, 6, 7, 8, 9]
     filename = f"Patterns with throws {throws}.txt"
     depth = 15
     valid_pairs_of_throws = [[a, b] for a in throws for b in throws if is_valid([a, b])]
     patterns_found = search_multiple_seeds(valid_pairs_of_throws, throws, [has_pass, no_dragon, no_repeated_pairs], depth=depth)
     sorted_by_length = sorted(list(patterns_found), key=lambda x: len(x), reverse=True)
-    with open(f"generate_patterns/{filename}", "w+") as f:
+    with open(f"generated_patterns/{filename}", "w+") as f:
         f.writelines(f"{pattern}\n" for pattern in sorted_by_length)
